f199c48a818105cd0aa2292de65fa4951949df28
Norman François <normanfrancois2004@gmail.com>
2025-12-15 20:08:21 +0100

    feat : ajout du déploiement automatique

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  output: 'export',
+  basePath: '/controverse',
+  assetPrefix: '/controverse',
+  trailingSlash: true,
 }
 
 export default nextConfig
